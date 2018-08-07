8c51eb1e60a3cf1c7b648ffa92e98ad67c8a7856
gecata21 <georgi.d.d97@gmail.com>
2018-08-07 09:47:42 +0300

    First page half

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  // Hamburger menu
+  var cross = $('.cross');
+  var hamburgerBtn = $('.hamburger');
+
+  hamburgerBtn.on('click', function () {
+    hamburgerBtn.hide(500);
+    cross.delay(350).show(500);
+  });
+
+  cross.on('click', function () {
+    hamburgerBtn.delay(350).show(500);
+    cross.hide(500);
+  });
+  // Slider for projects
+  $('#projectsSection-slick').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  // Slider for testimonials
+  $('#testimonialsSection-slick').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
