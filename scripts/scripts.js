18080a7474cd382e10cfaa5f457bcaef8ded737a
gecata21 <georgi.d.d97@gmail.com>
2018-08-17 22:35:35 +0200

    Tablet version ready and part of Desktop

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   var cross = $('.cross');
   var hamburgerBtn = $('.hamburger');
   var html = $('html');
-
+  
   hamburgerBtn.on('click', function () {
     hamburgerBtn.hide(500);
     cross.delay(350).show(500);
@@ -12,35 +12,68 @@ $(document).ready(function () {
       e.preventDefault()
     });
   });
-
+  
   cross.on('click', function () {
     hamburgerBtn.delay(350).show(500);
     cross.hide(500);
     html.removeClass('stop-scrolling');
     $('body, html').unbind('touchmove touchstart');
   });
+  
   // Slider for projects
   $('.projects-section--slick').slick({
     centerMode: true,
-    slidesToShow: 1
-  });
-    // Slider for certificates
-    $('.aboutpage__section2--slick').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      infinite: true,
-      centerMode: true,
-      responsive: [
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true
-          }
+    slidesToShow: 1,
+    centerPadding: '50px',
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          centerMode: true,
+          centerPadding: '80px',
+          slidesToShow: 1,
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          centerMode: false,
+          slidesToShow: 3,
+          centerPadding: '100px',
+          useCss: false
+        }
+      }
+    ]
+  });
+  // Slider for certificates
+  $('.aboutpage__section2--slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          centerMode: true,
+          centerPadding: '240px'
+        }
+      }
+    ]
+  });
   // Slider for testimonials
   $('.testimonials-section--slick').slick({
     infinite: true,
@@ -48,16 +81,16 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1
   });
-
+  
   // Modal images 
-
+  
   // Get the modal
   var modal = $('.skillspage__gallery--modal');
-
+  
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   var img = $('.skillspage__gallery img');
   var modalImg = $("#skillspage__gallery--modal-content");
-
+  
   $(img).click(function () {
     modal.css('display', 'block');
     var imageSource = $(this).attr('src');
@@ -67,14 +100,14 @@ $(document).ready(function () {
       e.preventDefault()
     });
   })
-
+  
   // Get the <span> element that closes the modal
   var galleryCross = $('.skillspage__gallery--modal-cross');
-
+  
   // When the user clicks on galleryCross, close the modal
   galleryCross.click(function () {
     modal.css('display', 'none')
-        html.removeClass('stop-scrolling');
+    html.removeClass('stop-scrolling');
     $('body, html').unbind('touchmove touchstart');
   });
 });
