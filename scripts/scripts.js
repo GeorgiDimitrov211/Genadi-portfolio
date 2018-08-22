42a1368482fa797885f0dd80467344e5fb944317
gecata21 <georgi.d.d97@gmail.com>
2018-08-22 20:36:43 +0200

    Some small details fixed.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,7 +67,8 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          variableWidth: true
+          variableWidth: true,
+          dots:true
         }
       }
     ]
